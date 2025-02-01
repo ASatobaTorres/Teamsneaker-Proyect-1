e65047197570bb2c221807433eef4dbaf54e5976
ASatobaTorres <andessatoba0@gmail.com>
2025-02-01 00:29:47 -0500

    Info Sneakers

--- js/code.js
+++ js/code.js
@@ -1,3 +1,13 @@
+const zapatillas = ["Nike", "Adidas", "Asics", "Puma", "Reebock", 
+                    "Spezial", "Caterpillar",  ]   
 
+const infoZapatillas = {nameSneakers: "Adidas", price: "17e4", type : "Running",
+                        ageWomen: [34,35,36,37,38,39], ageGentleman: [40,41,42,43,44,45] };
 
-<button type="boton" onclick = "document.getElementById('Marcas').innerHTML='Marquitas'"> Marcas </button>
+let envio = 16500; 
+// [e] es para exponencial
+let valor = 18e4 + envio + " " + "CON ENVIO";
+
+let bigNumber = BigInt("Tambien puedes comprar muchos mas estilos en TEAMSNEAKERS27")
+
+console.log(valor);
