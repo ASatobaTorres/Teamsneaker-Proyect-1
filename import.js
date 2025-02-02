242098b7896f8d3f1eb80e76bcf21eb440a09162
ASatobaTorres <andessatoba0@gmail.com>
2025-02-02 00:38:18 -0500

    Cambios basicos, corregir las importaciones

---
+++ import.js
@@ -0,0 +1,7 @@
+import { styles} from "./styles";
+import {object, code} from "./js";
+
+
+object();
+code();
+styles();
--- js/code.js
+++ js/code.js
@@ -1,13 +1,17 @@
-const zapatillas = ["Nike", "Adidas", "Asics", "Puma", "Reebock", 
-                    "Spezial", "Caterpillar",  ]   
+import { zapatillas } from "./object";
 
-const infoZapatillas = {nameSneakers: "Adidas", price: "17e4", type : "Running",
-                        ageWomen: [34,35,36,37,38,39], ageGentleman: [40,41,42,43,44,45] };
+// let bigNumber = BigInt("Tambien puedes comprar muchos mas estilos en TEAMSNEAKERS27");
+// console.log(valor);
 
+const infoZapatillas = zapatillas;
 let envio = 16500; 
-// [e] es para exponencial
 let valor = 18e4 + envio + " " + "CON ENVIO";
 
-let bigNumber = BigInt("Tambien puedes comprar muchos mas estilos en TEAMSNEAKERS27")
+function priceOnSend (envio) {
+    let priceTotal = envio + valor + infoZapatillas;
+    
+    return priceTotal   };
 
-console.log(valor);
+console.log(priceOnSend(16500, 175e3));
+
+
